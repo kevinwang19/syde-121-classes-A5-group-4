27dd5c863c7828dba13ba8b0944d3cd6ac42bfa1
Nick-palmar <66019522+Nick-palmar@users.noreply.github.com>
2020-11-16 14:37:29 -0500

    Update a5_book.cpp

--- a5_book.cpp
+++ a5_book.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include "a5_book.hpp"
+using namespace std;
 
+// book constructor implementations
+Book::Book() : 
+    title("n/a"), authors("n/a"), dop("n/a") {}
+
+Book::Book(string new_title) :
+    title(new_title), authors("n/a"), dop("n/a") {}
+
+Book::Book(string new_title, string new_authors) :
+    title(new_title), authors(new_authors), dop("n/a") {}
+
+Book::Book(string new_title, string new_authors, string new_dop) :
+    title(new_title), authors(new_authors), dop("n/a") {}
+
+// book service method
+void Book::print() {
+    // output the book info to console
+    cout << "The book title is: " << title << endl;
+    cout << "The book authors is/are: " << authors << endl;
+    cout << "The book date of publication is: " << dop << endl;
+}
