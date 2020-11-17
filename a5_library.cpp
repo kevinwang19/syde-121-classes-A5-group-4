8041a666e96a205f344f5b74c1819aa3589e4e63
Kevin Wang <70813550+kevinwang19@users.noreply.github.com>
2020-11-17 00:29:32 -0500

    Update a5_library.cpp

--- a5_library.cpp
+++ a5_library.cpp
@@ -1,1 +1,16 @@
-
+#include "a5_library.hpp"
+#include <iostream>
+using namespace std;
+ 
+Library::Library() {}
+ 
+Library::Library(vector<Book> new_book_list) :
+    book_list(new_book_list.begin(), new_book_list.end()){
+}
+    
+void Library::print() {
+    for (int index = 0; index < book_list.size(); index++) {
+        cout << book_list[index].print();
+    }
+    
+}
